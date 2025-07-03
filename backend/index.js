b9defd5935dd357a1f8f0b10f2395b504bd158a2
Anamika <anamikagarg2906@gmail.com>
2025-07-04 00:20:04 +0530

    fix deployment

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,6 @@ const { GoogleGenerativeAI } = require('@google/generative-ai');
 require('dotenv').config();
 
 const app = express();
-
 app.use(express.json());
 app.use(cors({
   origin: `${process.env.FRONTEND_URL}`,
